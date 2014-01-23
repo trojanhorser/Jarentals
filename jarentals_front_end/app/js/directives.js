b4a263cdd6b817e060fdd4b6f45b0738017fca6f
Denis Bell <denisdbell@gmail.com>
2014-01-23 09:55:32 -0400

    directive for facebookauth plugin

--- jarentals_front_end/app/js/directives.js
+++ jarentals_front_end/app/js/directives.js
@@ -3,7 +3,12 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
+JArentalsApplication.
+  directive('faceBookLoginButton',function(){
+	return {
+		template: "<fb:login-button show-faces='true' width='200' max-rows='1'></fb:login-button>"
+	}
+  }).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
