de7110798fdf72257d5619c30601202b194f4139
Denis Bell <denisdbell@gmail.com>
2014-01-24 02:03:25 -0400

    added partial for login and main page

--- jarentals_front_end/app/js/app.js
+++ jarentals_front_end/app/js/app.js
@@ -2,4 +2,11 @@
 
 
 // Declare app level module which depends on and services controllers
-var JArentalsApplication = angular.module('JArentalsApplication',[]);
+var JArentalsApplication = angular.module('JArentalsApplication',["ngRoute"]);
+
+JArentalsApplication.config(function($routeProvider) {
+  $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginController'});
+  $routeProvider.when('/main', {templateUrl: 'partials/main.html', controller: 'LoginController'});
+  $routeProvider.otherwise({redirectTo: '/view1'});
+});
+
