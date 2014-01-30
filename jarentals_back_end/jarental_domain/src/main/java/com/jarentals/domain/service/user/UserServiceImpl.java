418af994bcf5d7f18ca8c2649a5b7fc791902d82
Denis Bell <denisdbell@gmail.com>
2014-01-30 05:12:06 -0400

    added depedency management for projects through maven

--- jarentals_back_end/jarental_domain/src/main/java/com/jarentals/domain/service/user/UserServiceImpl.java
+++ jarentals_back_end/jarental_domain/src/main/java/com/jarentals/domain/service/user/UserServiceImpl.java
@@ -1,23 +1,25 @@
 package com.jarentals.domain.service.user;
 
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jarentals.common.util.Util;
 import com.jarentals.domain.User;
+import com.jarentals.domain.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
 	
 	@Autowired
-    private SessionFactory session;
+	UserRepository userRepository;
 	
 	public void addUser(User user) {
 		
 		if(Util.isObjectEmpty(user)){
 			throw new IllegalArgumentException();
+			
 		}
+		userRepository.save(user);
 	}
 
 }
