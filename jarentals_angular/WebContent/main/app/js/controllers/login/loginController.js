49f09963781a52a1a6e9d0f355a00ed9e171aab2
denisdbell <denisdbell@denisdbell-ThinkPad-T430>
2014-03-27 23:20:26 -0500

    login

--- jarentals_angular/WebContent/main/app/js/controllers/login/loginController.js
+++ jarentals_angular/WebContent/main/app/js/controllers/login/loginController.js
@@ -3,10 +3,12 @@
 /* Controllers */
 JArentalsApplication.controller('LoginController',function($scope,UserService,$FB,$location){
 		
-	//Local varibales	
-	var user = new UserService;	
-
-	updateLoginStatus(updateApiMe);
+	$scope.render = false;
+	$scope.username = null;
+	$scope.password = null;
+	$scope.email = null;
+	
+	//updateLoginStatus(updateApiMe);
 	
 	$scope.getFaceBookUserData = function(){
 		 $FB.api('/me', function (res) {
@@ -18,58 +20,37 @@ JArentalsApplication.controller('LoginController',function($scope,UserService,$F
 	    $FB.login(function (res) {
 	      if (res.authResponse) {
 	    	updateLoginStatus(updateApiMe);
-	    	//Check if User Exist
-	    	UserService.get(
-	    			
-	    		{subresource:"facebook",value:$scope.facebookUser.id},
-		    	
-	    		function(user){
-	    			//If User exits route them to the main page
-		    		console.log("user exists");
-		    	},function(err){
-		    		console.log("user does not exists");
-		    		//If user does not exist save them and route them to the main page
-		    		user.username = $scope.facebookUser.first_name;
-		    		user.fbId =  $scope.facebookUser.id;
-		  	        user.email =  $scope.facebookUser.email;
-		  	        user.$save();
-		    	}
-	    	);
 	      }
+	    },function(err){
+	    	console.log("Facebook error has occured");
 	    }, {scope: 'email,user_likes'});
 	}
 	
-	$scope.registerRegularUser = function(){
-		
-		UserService.get(
-	    			
-	    		{subresource:"email",value:$scope.email},
-		    	
-	    		function(user){
-	    			//If User exit provide error message
-		    	},function(err){
-		    		console.log("user does not exists");
-		    		//If user does not exist save them and route them to the main page
-		    		user.username = $scope.username;
-		    		user.password =  $scope.password;
-		    		user.email =  $scope.email;
-		    		console.log($scope.email);
-		  	        user.$save();
-		    	}
-	    	);
+	$scope.registerUser = function(){
+		var user = new UserService;	
+		user.username = $scope.username;
+		user.password = $scope.password;
+		user.email = $scope.email;
+	    user.$save();		
 	}
 	
 	function updateLoginStatus (more) {
 	    $FB.getLoginStatus(function (res) {
 	      $scope.loginStatus = res;
-
+	      $scope.render = true;
 	      (more || angular.noop)();
 	    });
 	 }
 	
-	function updateApiMe () {
+	function updateApiMe (loginFaceBookUser) {
 	    $FB.api('/me', function (res) {
-	      $scope.facebookUser = res;
+	    //if($scope.loginStatus.status == "connected"){
+		      $scope.facebookUser = res;
+		  	  $scope.username = $scope.facebookUser.first_name;
+		      $scope.email = $scope.facebookUser.email;	
+			  $scope.password = $scope.facebookUser.id;
+			  $scope.registerUser();
+	      //}
 	    });
-	  }
+	}
 });
